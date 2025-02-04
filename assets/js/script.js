f3cbc1cfa42aa4ba576bf578263c9930a2039c59
olutobi Adebayo <olutobi1996@icloud.com>
2025-02-04 11:32:46 +0000

    quiz questions added

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Quiz Questions
+const questions = [
+    {
+      category: "Sports",
+      questionText: "What football team won the World Cup in 1966?",
+      options: ["Brazil", "England", "France", "Italy"],
+      answer: "England",
+      hint: "It's a European team."
+    },
+    {
+      category: "Olympics",
+      questionText: "How many colored rings are in the Olympic Flag?",
+      options: ["Two", "Three", "Four", "Five"],
+      answer: "Five",
+      hint: "Think of the continents."
+    },
+    {
+      category: "Movies",
+      questionText: "In 'The Lion King', what animals trample Mufasa?",
+      options: ["Sheep", "Hyenas", "Wildebeests", "Bears"],
+      answer: "Wildebeests",
+      hint: "They are large herd animals."
+    },
+    {
+      category: "Movies",
+      questionText: "Who directed the film 'Pulp Fiction'?",
+      options: ["Daniel Day-Lewis", "Steven Spielberg", "Martin Scorsese", "Quentin Tarantino"],
+      answer: "Quentin Tarantino",
+      hint: "Famous for nonlinear storytelling."
+    },
+  ];
